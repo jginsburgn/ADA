67507db7e748facb359eb88e7871fe407a1f582a
Jonathan Ginsburg <jginsburgn@gmail.com>
2015-09-02 23:34:45 -0500

    All Sorting Algorithms added

---
+++ Xcode/AllSortingAlgorithms/AllSortingAlgorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AllSortingAlgorithms
+//
+//  Created by Jonathan Ginsburg on 8/30/15.
+//  Copyright (c) 2015 Jonathan Ginsburg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Xcode/BrickSort/BrickSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BrickSort
+//
+//  Created by Jonathan Ginsburg on 8/31/15.
+//  Copyright (c) 2015 Jonathan Ginsburg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
