d78b344a72780ee7bafae40861bcf9ddef80374a
Jonathan Ginsburg <jginsburgn@gmail.com>
2015-09-12 18:03:58 -0500

    BTreeMemoryAttempt's Insertion Method Working Correctly

---
+++ Xcode/BTreeMemoryAttempt/BTreeMemoryAttempt/BNode.h
@@ -0,0 +1,13 @@
+//
+//  BNode.h
+//  BTreeMemoryAttempt
+//
+//  Created by Jonathan Ginsburg on 9/11/15.
+//  Copyright (c) 2015 Jonathan Ginsburg. All rights reserved.
+//
+
+#ifndef BTreeMemoryAttempt_BNode_h
+#define BTreeMemoryAttempt_BNode_h
+
+
+#endif
---
+++ Xcode/BTreeMemoryAttempt/BTreeMemoryAttempt/BTree.h
@@ -0,0 +1,13 @@
+//
+//  BTree.h
+//  BTreeMemoryAttempt
+//
+//  Created by Jonathan Ginsburg on 9/11/15.
+//  Copyright (c) 2015 Jonathan Ginsburg. All rights reserved.
+//
+
+#ifndef BTreeMemoryAttempt_BTree_h
+#define BTreeMemoryAttempt_BTree_h
+
+
+#endif
