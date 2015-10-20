083999dacb71e4e2071166b2fb521d44f970cda7
Jonathan Ginsburg <jginsburgn@gmail.com>
2015-10-20 00:59:01 -0500

    Renamed Xcode to Xcode ADA Workspace; Added Snap 2.4 External Directory to ADA Workspace as tasks on this library are being done by modifying Snap-2.4/examples/testgraph/testgraph.cpp

---
+++ Snap-2.4/examples/test/test/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Jonathan Ginsburg on 10/19/15.
+//  Copyright © 2015 Jonathan Ginsburg. All rights reserved.
+//
+
+#include <iostream>
+#include "/Users/MacBook/Desktop/Snap-2.4/snap-core"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
