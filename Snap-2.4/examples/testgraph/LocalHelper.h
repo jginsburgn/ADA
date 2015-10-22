11a4cd8dd0d5ffc38408e0f8d618d6877835b876
Jonathan Ginsburg <jginsburgn@gmail.com>
2015-10-22 03:35:01 -0500

    Just missing Floyd-Warshall's Algorithm

--- Snap-2.4/examples/testgraph/LocalHelper.h
+++ Snap-2.4/examples/testgraph/LocalHelper.h
@@ -12,14 +12,75 @@
 #include <time.h>
 
 //Local Helper Implementation
+
+//Constants and global variables declaration:
 const std::string EMPTY_STRING = "";
 time_t begin, end;
 
+//Conversions:
+std::string DoubleToString(double value) {
+    char buffer[32];
+    snprintf(buffer,32,"%f",value);
+    std::string retVal = buffer;
+    return retVal;
+}
+std::string IntToString(int value){
+    char buffer[32];
+    snprintf(buffer,32,"%i",value);
+    std::string retVal = buffer;
+    return retVal;
+}
+
+//Accessors:
+int GetInfo(const TNEANet::TNodeI & node){
+    TIntV attributes;
+    node.GetIntAttrVal(attributes);
+    return attributes[0];
+}
+int GetInfo(const TNEANet::TEdgeI & edge){
+    TIntV attributes;
+    edge.GetIntAttrVal(attributes);
+    return attributes[0];
+}
+
+//Printing output:
 void Print(const std::string & message, bool addLineBreak = false){
     std::string processedMessage = message;
     if (addLineBreak) processedMessage.append("\n");
     printf(processedMessage.c_str());
+    fflush(stdout);
 }
+void PrintEmptyLine(){
+    Print("", true);
+}
+void PrintIntVector(const TIntV & vector){
+    for (int i = 0; i < vector.Len(); ++i) {
+        Print(IntToString(vector[i]));
+        if (i == vector.Len() - 1) {
+            Print(".");
+        }
+        else Print(", ");
+    }
+}
+
+//Descriptors:
+std::string GetEdgeDescription(const TNEANet::TEdgeI & edge){
+    std::string description = "Edge's id: ";
+    description.append(IntToString(edge.GetId()));
+    description.append(", ");
+    description.append(IntToString(edge.GetSrcNId()));
+    description.append(" -> ");
+    description.append(IntToString(edge.GetDstNId()));
+    description.append(", with weight: ");
+    description.append(IntToString(GetInfo(edge)));
+    return description;
+}
+std::string GetEdgeDescription(const int & Id, const TNEANet & graph){
+    TNEANet::TEdgeI edge = graph.GetEI(Id);
+    return GetEdgeDescription(edge);
+}
+
+//Reading input:
 std::string ReadWord(const std::string & prompt) {
     if (prompt != "") {
         Print(prompt);
@@ -38,24 +99,20 @@ int ReadInt(const std::string & prompt) {
     return retVal;
 }
 
+//Verifications:
+bool IsIntInVector(const int & value, const TIntV & vector){
+    for (int i = 0; i < vector.Len(); ++i) {
+        if (value == vector[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Chronometer actions:
 void StartChronometer(){
     begin = time(NULL);
 }
-
-std::string DoubleToString(double value) {
-    char buffer[32];
-    snprintf(buffer,32,"%f",value);
-    std::string retVal = buffer;
-    return retVal;
-}
-
-std::string IntToString(int value){
-    char buffer[32];
-    snprintf(buffer,32,"%i",value);
-    std::string retVal = buffer;
-    return retVal;
-}
-
 std::string ChronometerLap(){
     //    printf ("It took me %d clicks (%f seconds).\n", t, ((float)t)/CLOCKS_PER_SEC);
     //    double num = ((float)t)/CLOCKS_PER_SEC;
@@ -66,6 +123,68 @@ std::string ChronometerLap(){
     double elapsedTime = difftime(end, begin);
     std::string retVal = DoubleToString(elapsedTime);
     return retVal;
+}
+
+//Stack LIFO operations:
+void InsertInStack(TIntV & stack, const int value){
+    stack.Add(value);
+}
+int PopFromStack(TIntV & stack){
+    int retVal = stack.Last();
+    stack.DelLast();
+    return retVal;
+}
+
+//Prim's Priority Queue operations:
+void PrimEnqueue(TIntV & queue, const int & value, const PNEANet & graph){
+    if (IsIntInVector(value, queue)) return;
+    if (queue.Empty()) {
+        queue.Add(value);
+    }
+    else{
+        int index = 0;
+        TNEANet::TEdgeI edgeToAdd = graph->GetEI(value);
+        while (GetInfo(edgeToAdd) > GetInfo(graph->GetEI(queue[index]))) { ++index; }
+        queue.Ins(index, value);
+    }
+}
+int PrimDequeue(TIntV & queue){
+    int retVal = queue[0];
+    queue.Del(0);
+    return retVal;
+}
+
+//Graph Insertion & Deletion:
+void InsertNode(PNEANet & graph, const int value){
+    if (graph->IsNode(value)) Print("That vertex already exists.", true);
+    else {
+        graph->AddNode(value);
+        graph->AddIntAttrDatN(value, value, "");
+    }
+}
+void DeleteNode(PNEANet & graph, const int nodesId){
+    if (!graph->IsNode(nodesId)) {
+        Print("Node is not in the graph", true);
+    }
+    else graph->DelNode(nodesId);
+}
+void InsertEdge(PNEANet & graph, const int originNodesId, const int destinationNodesId, const int weight){
+    if (!graph->IsNode(originNodesId) || !graph->IsNode(destinationNodesId)) Print("Origin or destination does not exist", true);
+    else {
+        int edgesId = graph->AddEdge(originNodesId, destinationNodesId);
+        graph->AddIntAttrDatE(edgesId, weight, "");
+    }
+}
+void ForceEdgeInsertion(PNEANet & graph, const int originNodesId, const int destinationNodesId, const int weight){
+    if (!graph->IsNode(originNodesId)) InsertNode(graph, originNodesId);
+    if (!graph->IsNode(destinationNodesId)) InsertNode(graph, destinationNodesId);
+    InsertEdge(graph, originNodesId, destinationNodesId, weight);
+}
+void DeleteEdge(PNEANet & graph, const int edgesId){
+    if (!graph->IsEdge(edgesId)) {
+        Print("Edge is not in the graph", true);
+    }
+    else graph->DelEdge(edgesId);
 }
 
 //End of Local Helper
--- Snap-2.4/examples/testgraph/testgraph.cpp
+++ Snap-2.4/examples/testgraph/testgraph.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "LocalHelper.h"
 
+/*
 //Node traversal
 //for (TNEANet::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++){
 //    TIntV attributes;
@@ -8,33 +9,294 @@
 //    std::string firstMessage = "Node with info: ";
 //    firstMessage.append(IntToString(attributes[0]));
 //    Print(firstMessage, true);
-//    printf("\tNode's Id: %i, Node's out degree: %d, Node's in degree: %d\n", NI.GetId(), NI.GetOutDeg(), NI.GetInDeg());
+//    printf("\tNode's Id: %i, Node's out degree: %d, Nobde's in degree: %d\n", NI.GetId(), NI.GetOutDeg(), NI.GetInDeg());
 //}
+ */
 
-void PrintGStats(PNEANet Graph) {
-    printf("Stats: nodes %d, edges %d, empty %s\n",
-           Graph->GetNodes(), Graph->GetEdges(),
-           Graph->Empty() ? "yes" : "no");
+//Printing operations:
+void PrintEdges(const PNEANet & graph){
+    Print("Edge's list: ", true);
+    for (TNEANet::TEdgeI EI = graph->BegEI(); EI < graph->EndEI(); EI++){
+        printf("\t%s\n", GetEdgeDescription(EI).c_str());
+    }
+}
+void PrintNodes(const PNEANet & graph){
+    Print("Node's list: ");
+    int counter = 1;
+    for (TNEANet::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++){
+        std::string info = IntToString(GetInfo(NI));
+        if (counter++ < graph->GetNodes()) info.append(", ");
+        else info.append(".");
+        Print(info);
+    }
+    PrintEmptyLine();
+}
+void PrintGraphDetails(const PNEANet & graph) {
+    printf("Details: \n\tNumber of nodes %d, number of edges %d...",
+           graph->GetNodes(), graph->GetEdges());
+    PrintEmptyLine();
+    if (!graph->Empty()) {
+        PrintNodes(graph);
+        PrintEdges(graph);
+    }
 }
 
+//Saving and loading:
+void SaveGraph(const PNEANet & graph){
+    TSnap::SaveEdgeListNet(graph, ReadWord("Enter name of graph to save (single word): ").c_str(), "");
+}
+void LoadGraph(PNEANet & graph){
+    graph = TSnap::LoadEdgeListNet(ReadWord("Enter name of graph to load (single word): ").c_str(), '\t');
+}
+
+//Depth First Traversing:
+void DFSExploreNode(TIntV & visitedNodes, TIntV & edgesStack, const TNEANet::TNodeI & node, const PNEANet & graph){
+    if (!IsIntInVector(node.GetId(), visitedNodes)) visitedNodes.Add(node.GetId());
+    for (int e = 0; e < node.GetOutDeg(); e++){
+        int edgesId = node.GetOutEId(e);
+        TNEANet::TEdgeI edge = graph->GetEI(edgesId);
+        int destinationNodesIdOfEdge = edge.GetDstNId();
+        if (!IsIntInVector(destinationNodesIdOfEdge, visitedNodes)) {
+            InsertInStack(edgesStack, edgesId);
+            visitedNodes.Add(destinationNodesIdOfEdge);
+        }
+    }
+}
+void DepthFirstTraversing(const PNEANet & graph, int sourceNodeId){
+    if (!graph->IsNode(sourceNodeId)) {
+        Print("Node is not in the graph...", true);
+        return;
+    }
+    TIntV visitedNodes;
+    TIntV edgesStack;
+    TNEANet::TNodeI currentNode = graph->GetNI(sourceNodeId);
+    DFSExploreNode(visitedNodes, edgesStack, currentNode, graph);
+    printf("\tConducting DFS from node with id: %i\n", currentNode.GetId());
+    while (!edgesStack.Empty()) {
+        TNEANet::TEdgeI edgeToExplore = graph->GetEI(PopFromStack(edgesStack));
+        currentNode = graph->GetNI(edgeToExplore.GetDstNId());
+        printf("\t\tArriving to: %i, from edge: %s.\n", currentNode.GetId(), GetEdgeDescription(edgeToExplore).c_str());
+        DFSExploreNode(visitedNodes, edgesStack, currentNode, graph);
+    }
+}
+
+//Breadth First Traversing:
+void BFSExploreNode(TIntV & visitedNodes, TSnapQueue<int> & edgesQueue, const TNEANet::TNodeI & node, const PNEANet & graph){
+    if (!IsIntInVector(node.GetId(), visitedNodes)) visitedNodes.Add(node.GetId());
+    for (int e = 0; e < node.GetOutDeg(); e++){
+        int edgesId = node.GetOutEId(e);
+        TNEANet::TEdgeI edge = graph->GetEI(edgesId);
+        int destinationNodesIdOfEdge = edge.GetDstNId();
+        if (!IsIntInVector(destinationNodesIdOfEdge, visitedNodes)) {
+            edgesQueue.Push(edgesId);
+            visitedNodes.Add(destinationNodesIdOfEdge);
+        }
+    }
+}
+void BreadthFirstTraversing(const PNEANet & graph, int sourceNodeId){
+    if (!graph->IsNode(sourceNodeId)) {
+        Print("Node is not in the graph...", true);
+        return;
+    }
+    TIntV visitedNodes;
+    TSnapQueue<int> edgesQueue;
+    TNEANet::TNodeI currentNode = graph->GetNI(sourceNodeId);
+    BFSExploreNode(visitedNodes, edgesQueue, currentNode, graph);
+    printf("\tConducting BFS from node with id: %i\n", currentNode.GetId());
+    while (!edgesQueue.Empty()) {
+        TNEANet::TEdgeI edgeToExplore = graph->GetEI(edgesQueue.Top());
+        edgesQueue.Pop();
+        currentNode = graph->GetNI(edgeToExplore.GetDstNId());
+        printf("\t\tArriving to: %i, from edge: %s.\n", currentNode.GetId(), GetEdgeDescription(edgeToExplore).c_str());
+        BFSExploreNode(visitedNodes, edgesQueue, currentNode, graph);
+    }
+}
+
+//Prim's Minimum Expansion:
+void PrimsExploreNode(TIntV & visitedNodes, TIntV & edgesQueue, const TNEANet::TNodeI & node, const PNEANet & graph){
+    if (!IsIntInVector(node.GetId(), visitedNodes)) visitedNodes.Add(node.GetId());
+    for (int e = 0; e < node.GetOutDeg(); e++){
+        int edgesId = node.GetOutEId(e);
+        TNEANet::TEdgeI edge = graph->GetEI(edgesId);
+        int destinationNodesIdOfEdge = edge.GetDstNId();
+        if (!IsIntInVector(destinationNodesIdOfEdge, visitedNodes)) {
+            PrimEnqueue(edgesQueue, edgesId, graph);
+        }
+    }
+}
+PNEANet PrimsMinimumExpansionTree(const PNEANet & graph, const int sourceNodeId){
+    PNEANet minimumExpansionTree = TNEANet::New();
+    if (!graph->IsNode(sourceNodeId)) {
+        Print("Node is not in the graph...", true);
+        return minimumExpansionTree;
+    }
+    InsertNode(minimumExpansionTree, sourceNodeId);
+    TIntV visitedNodes;
+    TIntV edgesQueue;
+    TNEANet::TNodeI currentNode = graph->GetNI(sourceNodeId);
+    PrimsExploreNode(visitedNodes, edgesQueue, currentNode, graph);
+    while (visitedNodes.Len() < graph->GetNodes()) {
+        TNEANet::TEdgeI edgeToExplore = graph->GetEI(PrimDequeue(edgesQueue));
+        ForceEdgeInsertion(minimumExpansionTree, edgeToExplore.GetSrcNId(), edgeToExplore.GetDstNId(), GetInfo(edgeToExplore));
+        currentNode = graph->GetNI(edgeToExplore.GetDstNId());
+        PrimsExploreNode(visitedNodes, edgesQueue, currentNode, graph);
+    }
+    return minimumExpansionTree;
+}
+
+//Kruskal's Minimum Expansion:
+void BuildSingletonClusters(TVec<TIntV> & clusters, const PNEANet & graph){
+    for (TNEANet::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++){
+        TIntV newCluster;
+        newCluster.Add(NI.GetId());
+        clusters.Add(newCluster);
+    }
+}
+void EnqueueAllEdges(TIntV & queue, const PNEANet & graph){
+    for (TNEANet::TEdgeI EI = graph->BegEI(); EI < graph->EndEI(); EI++){
+        PrimEnqueue(queue, EI.GetId(), graph);
+    }
+}
+void MergeClusters(TVec<TIntV> & clusters, const int indexOfClusterOne, const int indexOfClusterTwo){
+    TIntV clusterOne = clusters[indexOfClusterOne];
+    TIntV clusterTwo = clusters[indexOfClusterTwo];
+    for (int i = 0; i < clusterTwo.Len(); ++i) {
+        clusters[indexOfClusterOne].Add(clusterTwo[i]);
+    }
+    clusters.Del(indexOfClusterTwo);
+}
+int GetClustersIndexWhichContains(TVec<TIntV> & clusters, const int nodeId){
+    for (int i = 0; i < clusters.Len(); ++i) {
+        if (IsIntInVector(nodeId, clusters[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+PNEANet KruskalMinimumExpansionTree(const PNEANet & graph, const int sourceNodeId){
+    PNEANet minimumExpansionTree = TNEANet::New();
+    if (!graph->IsNode(sourceNodeId)) {
+        Print("Node is not in the graph...", true);
+        return minimumExpansionTree;
+    }
+    TVec<TIntV> clusters;
+    BuildSingletonClusters(clusters, graph);
+    TIntV edgesQueue;
+    EnqueueAllEdges(edgesQueue, graph);
+    while (clusters.Len() != 1) {
+        TNEANet::TEdgeI currentEdge = graph->GetEI(PrimDequeue(edgesQueue));
+        int indexOfClusterWithOrigin = GetClustersIndexWhichContains(clusters, currentEdge.GetSrcNId());
+        int indexOfClusterWithDestination = GetClustersIndexWhichContains(clusters, currentEdge.GetDstNId());
+        if (indexOfClusterWithOrigin != indexOfClusterWithDestination) {
+            MergeClusters(clusters, indexOfClusterWithOrigin, indexOfClusterWithDestination);
+            ForceEdgeInsertion(minimumExpansionTree, currentEdge.GetSrcNId(), currentEdge.GetDstNId(), GetInfo(currentEdge));
+        }
+    }
+    return minimumExpansionTree;
+}
+
+//Dijkstra's Shortest Paths:
+void EnqueueAllNodes(TIntV & queue, const PNEANet & graph){
+    for (TNEANet::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++){
+        queue.Add(NI.GetId());
+    }
+}
+int DequeueDijkstras(TIntV & queue, THash<TInt, TInt> & distance){
+    int minimumsIndex = 0;
+    for (int i = 0; i < queue.Len(); ++i) {
+        if (distance.IsKey(queue[i])) {
+            minimumsIndex = i;
+            break;
+        }
+    }
+    for (int i = 0; i < queue.Len(); ++i) {
+        if (distance.IsKey(queue[i])) {
+            if (distance.GetDat(queue[i]) < distance.GetDat(queue[minimumsIndex])) {
+                minimumsIndex = i;
+            }
+        }
+    }
+    int retVal = queue[minimumsIndex];
+    queue.Del(minimumsIndex);
+    return retVal;
+}
+void GetPathTo(std::string & path, const THash<TInt, TInt> & predecessor, const int destinationNodesId){
+    if (predecessor.IsKey(destinationNodesId)) {
+        GetPathTo(path, predecessor, predecessor.GetDat(destinationNodesId));
+        path.append(" -> ");
+    }
+    path.append(IntToString(destinationNodesId));
+}
+void DijkstrasShortestPaths(THash<TInt, TInt> & distance, THash<TInt, TInt> & predecessor, const PNEANet & graph, const int sourceNodeId){
+    if (!graph->IsNode(sourceNodeId)) {
+        Print("Node is not in the graph...", true);
+        return;
+    }
+    TIntV nodesQueue;
+    EnqueueAllNodes(nodesQueue, graph);
+    distance.AddDat(sourceNodeId) = 0;
+    while (!nodesQueue.Empty()) {
+        int currentNodesId = DequeueDijkstras(nodesQueue, distance);
+        TNEANet::TNodeI currentNode = graph->GetNI(currentNodesId);
+        for (int e = 0; e < currentNode.GetOutDeg(); e++){
+            int edgesId = currentNode.GetOutEId(e);
+            TNEANet::TEdgeI edge = graph->GetEI(edgesId);
+            if (!IsIntInVector(edge.GetDstNId(), nodesQueue)) {
+                continue;
+            }
+            if (distance.IsKey(edge.GetDstNId())) {
+                int alternativeDistance = distance.GetDat(currentNodesId) + GetInfo(edge);
+                if (alternativeDistance < distance.GetDat(edge.GetDstNId())) {
+                    distance.GetDat(edge.GetDstNId()) = alternativeDistance;
+                    predecessor.GetDat(edge.GetDstNId()) = currentNode.GetId();
+                }
+            }
+            else {
+                distance.AddDat(edge.GetDstNId()) = distance.GetDat(currentNodesId) + GetInfo(edge);
+                predecessor.AddDat(edge.GetDstNId()) = currentNode.GetId();
+            }
+        }
+    }
+}
+
+//Miscellaneous:
+void ShowOperationTime(int wait){
+    std::string timeMessage = "Operation completed in: ";
+    std::string time = ChronometerLap();
+    timeMessage.append(time);
+    timeMessage.append(" seconds");
+    Print(timeMessage);
+    for (int i = 0; i < wait; ++i) {
+        usleep(500000);
+        Print(".");
+    }
+    PrintEmptyLine();
+}
+
+//User Interaction:
+void PrintMenu(){
+    PrintEmptyLine();
+    Print("1) For Vertex Insertion", true);
+    Print("2) For Edge Insertion", true);
+    Print("3) For Vertex Deletion", true);
+    Print("4) For Edge Deletion", true);
+    Print("5) For Depth First Traversing", true);
+    Print("6) For Breadth First Traversing", true);
+    Print("7) For getting Minimum Expansion Tree of Graph by Prim's Algorithm", true);
+    Print("8) For getting Minimum Expansion Tree of Graph by Kruskal's Algorithm", true);
+    Print("9) For getting Shortest Paths from a given Vertex to all others by Dijkstra's Algorithm", true);
+    Print("10) For getting Shortest Paths from a given Vertex to all others by Floyd-Warshall's Algorithm", true);
+    Print("11) For printing Graph statistics", true);
+    Print("12) For Saving Current Graph", true);
+    Print("13) For Loading a Saved Graph", true);
+    Print("0) For finishing program", true);
+}
 int Prompt(){
     PNEANet graph = TNEANet::New();
     while (true) {
-        Print("", true);
-        Print("1) For Vertex Insertion", true);
-        Print("2) For Edge Insertion", true);
-        Print("3) For Vertex Deletion", true);
-        Print("4) For Edge Deletion", true);
-        Print("5) For Depth First Traversing", true);
-        Print("6) For Breadth First Traversing", true);
-        Print("7) For getting Minimum Expansion Tree of Graph by Prim's Algorithm", true);
-        Print("8) For getting Minimum Expansion Tree of Graph by Kruskal's Algorithm", true);
-        Print("9) For getting Shortest Paths from a given Vertex to all others by Dijkstra's Algorithm", true);
-        Print("10) For getting Shortest Paths from a given Vertex to all others by Floyd-Warshall's Algorithm", true);
-        Print("11) For printing Graph statistics", true);
-        Print("0) For finishing program", true);
+        PrintMenu();
         int choice = ReadInt("Enter your choice: ");
-        Print("", true);
+        PrintEmptyLine();
         StartChronometer();
         if (choice == 0) {
             Print("Process terminated...", true);
@@ -42,65 +304,91 @@ int Prompt(){
         }
         else if (choice == 1){
             int data = ReadInt("Enter info (integer) of vertex to add: ");
-            if (graph->IsNode(data)) Print("That vertex already exists.", true);
-            else {
-                graph->AddNode(data);
-                graph->AddIntAttrDatN(data, data, "");
-            }
+            InsertNode(graph, data);
         }
         else if (choice == 2){
-            Print("Node's list: ");
-            int counter = 1;
-            for (TNEANet::TNodeI NI = graph->BegNI(); NI < graph->EndNI(); NI++){
-                TIntV attributes;
-                NI.GetIntAttrVal(attributes);
-                std::string info = IntToString(attributes[0]);
-                if (counter++ < graph->GetNodes()) info.append(", ");
-                else info.append(".");
-                Print(info);
-            }
-            Print("", true);
+            PrintNodes(graph);
             int origin = ReadInt("Enter origin node: ");
             int destination = ReadInt("Enter destination node: ");
-            int edgesId = 0;
-            if (!graph->IsNode(origin) || !graph->IsNode(destination)) Print("Origin or destination does not exist", true);
-            else {
-                edgesId = graph->AddEdge(origin, destination);
-                graph->AddIntAttrDatE(edgesId, ReadInt("Enter the weight of the edge to add: "), "");
-            }
+            int weight = ReadInt("Enter the weight of the edge to add: ");
+            InsertEdge(graph, origin, destination, weight);
         }
         else if (choice == 3){
-            
+            if (graph->GetNodes() == 0) {
+                Print("Graph has no nodes...", true);
+            }
+            else {
+                PrintNodes(graph);
+                int nodeToDelete = ReadInt("Enter node to delete: ");
+                DeleteNode(graph, nodeToDelete);
+            }
         }
         else if (choice == 4){
-            
+            if (graph->GetEdges() == 0) {
+                Print("Graph has no edges...", true);
+            }
+            else {
+                PrintEdges(graph);
+                int edgeToDelete = ReadInt("Enter id of edge to delete: ");
+                DeleteEdge(graph, edgeToDelete);
+            }
         }
         else if (choice == 5){
-            
+            PrintNodes(graph);
+            DepthFirstTraversing(graph, ReadInt("Enter the source node's id for traversing: "));
         }
         else if (choice == 6){
-            
+            PrintNodes(graph);
+            BreadthFirstTraversing(graph, ReadInt("Enter the source node's id for traversing: "));
         }
         else if (choice == 7){
-            
+            PrintNodes(graph);
+            PNEANet minimumExpansionTree = PrimsMinimumExpansionTree(graph, ReadInt("Enter the source node's id for building minimum expansion tree with Prim's Algorithm: "));
+            PrintGraphDetails(minimumExpansionTree);
         }
         else if (choice == 8){
-            
+            PrintNodes(graph);
+            PNEANet minimumExpansionTree = KruskalMinimumExpansionTree(graph, ReadInt("Enter the source node's id for building minimum expansion tree with Kruskal's Algorithm: "));
+            PrintGraphDetails(minimumExpansionTree);
         }
         else if (choice == 9){
-            
+            PrintNodes(graph);
+            THash<TInt, TInt> distance;
+            THash<TInt, TInt> predecessor;
+            int sourceNodesId = ReadInt("Enter the source node's id for finding shortest paths from, using Dijkstra's Algorithm: ");
+            DijkstrasShortestPaths(distance, predecessor, graph, sourceNodesId);
+            int input = 0;
+            while (input != -1) {
+                input = ReadInt("Enter the destination node's id for showing shortest path and cost, or -1 to finish: ");
+                if (!graph->IsNode(input)) {
+                    continue;
+                }
+                std::string path;
+                GetPathTo(path, predecessor, input);
+                std::string message = "To get from ";
+                message.append(IntToString(sourceNodesId));
+                message.append(" to ");
+                message.append(IntToString(input));
+                message.append(" it costs: ");
+                message.append(IntToString(distance.GetDat(input)));
+                message.append(", and you need to follow this path:\n\t");
+                message.append(path);
+                Print(message, true);
+            }
         }
         else if (choice == 10){
             
         }
         else if (choice == 11){
-            PrintGStats(graph);
+            PrintGraphDetails(graph);
         }
-        std::string timeMessage = "Operation completed in: ";
-        std::string time = ChronometerLap();
-        timeMessage.append(time);
-        timeMessage.append(" seconds.");
-        Print(timeMessage, true);
+        else if (choice == 12){
+            SaveGraph(graph);
+        }
+        else if (choice == 13){
+            LoadGraph(graph);
+        }
+        ShowOperationTime(0);
     }
     return 0;
 }
